ae7cb2ece9d89f057cc66ca0726bcac371c1a637
comfuture <comfuture@gmail.com>
2021-12-12 06:04:13 +0000

    decorate sample output

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>{{ data?.message }}</div>
+  <div class="min-h-screen bg-gray-100 p-2">
+    <div class="bg-white rounded-md shadow-lg p-4">
+      <span class="font-bold">Message from server:</span>
+      <span>{{ data?.message }}</span>
+    </div>
+    <div class="text-center">Running on {{ runMode }}</div>
+  </div>
 </template>
 <script setup lang="ts">
 const { $api } = useNuxtApp()
 const { data } = await $api('/')
+const runMode = process.server ? 'server' : 'browser'
 </script>
